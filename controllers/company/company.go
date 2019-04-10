00518ac6fb0ec7b41bdfa38a22386bce30f304b9
15110139 <000big0bang000@gmail.com>
2019-04-11 00:40:31 +0700

    tien

---
+++ controllers/company/company.go
@@ -0,0 +1,83 @@
+package comapnycontrolers
+
+import (
+	"github.com/find-job-server-golang/service"
+	"github.com/gin-gonic/gin"
+	entities "github.com/find-job-server-golang/entites"
+	constant "github.com/find-job-server-golang/util/constant"
+	response "github.com/find-job-server-golang/util/response"
+
+)
+
+
+
+type CompanyControllers struct {
+
+}
+
+func (companyControllers *CompanyControllers) CreateCompany(c *gin.Context){
+
+	var companyObj entities.Company
+	c.ShouldBindJSON(&companyObj)
+	companyService := service.CompanyService{}
+	_, isNotFound := companyService.FindCompanyWithName(companyObj.Name)
+	if !isNotFound{
+		response.RespondWithError(c,constant.COMPANY_ALREADY_EXISTS,500)
+		return
+	}else{
+		newCompany:= companyService.CreateCompany(companyObj)
+		response.RespondSuccess(c,gin.H{
+			"company":newCompany,
+		},200)
+	}
+
+}
+
+
+func (companyControllers *CompanyControllers) UpdateCompany(c *gin.Context){
+
+	var dataCompanyUpdate entities.Company
+	c.ShouldBindJSON(&dataCompanyUpdate)
+	companyService := service.CompanyService{}
+	_, isNotFound := companyService.FindCompanyWithID(dataCompanyUpdate.CompanyId)
+	if isNotFound{
+		response.RespondWithError(c,constant.COMPANY_NOT_FOUND,500)
+		return
+	}else{
+		result:= companyService.UpdateCompanyWithID(dataCompanyUpdate.CompanyId,dataCompanyUpdate)
+		response.RespondSuccess(c,gin.H{
+			"company":result,
+		},200)
+	}
+
+}
+
+// func (companyControllers *CompanyControllers) RemoveCompany(c *gin.Context){
+
+// 	var companyId uuid.UUID
+// 	c.ShouldBindJSON(&companyId)
+// 	companyService := service.CompanyService{}
+// 	_, isNotFound := companyService.FindCompanyWithID(companyId)
+// 	if isNotFound{
+// 		response.RespondWithError(c,constant.COMPANY_NOT_FOUND,500)
+// 		return
+// 	}else{
+// 		result:= companyService.Re(dataCompanyUpdate.CompanyId,dataCompanyUpdate)
+// 		response.RespondSuccess(c,gin.H{
+// 			"company":result,
+// 		},200)
+// 	}
+
+// }
+
+
+func (companyControllers *CompanyControllers) Companies(c *gin.Context){
+	companyService := service.CompanyService{}
+	companies:= companyService.Companies()
+	response.RespondSuccess(c,gin.H{
+		"data":companies,
+	},200)
+	return
+}
+
+
---
+++ struct/company/company.go
@@ -0,0 +1,18 @@
+package structcompany
+
+// type People struct {
+// 	PeopleId uuid.UUID `json:"peopleId"`
+// 	Name      string
+// 	Position  string
+// 	Introduce string
+// }
+
+
+// type  DataUpdateCompany struct {
+// 	Name         string   `json:"name"`
+// 	Technologies []string `gorm: "technologies type:string[]"`
+// 	CompanyType  string   `json:"companyType"`
+// 	Location     string   `json:"location"`
+// 	Decs         string   `json:"decs"`
+// 	OurStore     string   `json:"ourStore"`
+// }
